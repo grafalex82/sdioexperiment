66ade1f6422b857a6cbd05a84045fd5114238293
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2022-04-22 12:15:45 +0300

    Test: wrap SD functionality into a class

--- Tests/test_connection.py
+++ Tests/test_connection.py
@@ -4,87 +4,92 @@
 import pytest
 import serial
 
+class SD:
+    def __init__(self):
+        self.port = serial.Serial('COM6', baudrate=115200)
+
+
+    def sendCommand(self, cmd):
+        self.port.write((cmd + "\n").encode())
+
+        while True:
+            line = self.port.readline().decode().rstrip()
+            print("Received: " + line)
+
+            if not line:
+                continue
+
+            resp, argument = line.split(' ', 1) if ' ' in line else (line, None)
+
+            if resp == "OK":
+                return argument
+
+            if resp == "ERROR":
+                raise RuntimeError(argument)
+
+
 @pytest.fixture(scope="session", autouse=True)
-def board():
-    yield serial.Serial('COM6', baudrate=115200)
+def sd():
+    yield SD()
 
 
-def sendCommand(port, cmd):
-    port.write((cmd + "\n").encode())
-
-    while True:
-        line = port.readline().decode().rstrip()
-        print("Received: " + line)
-
-        if not line:
-            continue
-
-        resp, argument = line.split(' ', 1) if ' ' in line else (line, None)
-
-        if resp == "OK":
-            return argument
-
-        if resp == "ERROR":
-            raise RuntimeError(argument)
+def test_loopback(sd):
+    sd.sendCommand("LOOPBACK")
 
 
-def test_loopback(board):
-    sendCommand(board, "LOOPBACK")
+def test_loopback_ok(sd):
+    sd.sendCommand("LOOPBACK OK")
 
 
-def test_loopback_ok(board):
-    sendCommand(board, "LOOPBACK OK")
-
-
-def test_loopback_error(board):
+def test_loopback_error(sd):
     with pytest.raises(RuntimeError):
-        sendCommand(board, "LOOPBACK ERROR")
+        sd.sendCommand("LOOPBACK ERROR")
 
 
-def test_loopback_error_msg(board):
+def test_loopback_error_msg(sd):
     with pytest.raises(RuntimeError) as err:
-        sendCommand(board, "LOOPBACK ERROR error message")
+        sd.sendCommand("LOOPBACK ERROR error message")
 
     assert(str(err.value) == "error message")
 
 
-def test_spi_init(board):
-    sendCommand(board, "SPI_INIT 256")
-    sendCommand(board, "RESET")
-    sendCommand(board, "CMD0")
-    v2card = sendCommand(board, "CMD8")
+def test_spi_init(sd):
+    sd.sendCommand("SPI_INIT 256")
+    sd.sendCommand("RESET")
+    sd.sendCommand("CMD0")
+    v2card = sd.sendCommand("CMD8")
     assert(v2card)
 
-    sendCommand(board, "CMD58")
+    sd.sendCommand("CMD58")
 
     status = "Busy"
     retries = 0
     while status == "Busy":
-        sendCommand(board, "CMD55")
-        status = sendCommand(board, "ACMD41 " + v2card)
+        sd.sendCommand("CMD55")
+        status = sd.sendCommand("ACMD41 " + v2card)
         retries += 1
         assert(retries < 10)
     assert(status == "Valid")
 
-    sdhc = sendCommand(board, "CMD58")
+    sdhc = sd.sendCommand("CMD58")
     assert(sdhc == "SDHC")
 
 
-def test_sdio_init(board):
-    sendCommand(board, "SDIO_INIT 256")
-    sendCommand(board, "RESET")
-    sendCommand(board, "CMD0")
-    v2card = sendCommand(board, "CMD8")
+def test_sdio_init(sd):
+    sd.sendCommand("SDIO_INIT 256")
+    sd.sendCommand("RESET")
+    sd.sendCommand("CMD0")
+    v2card = sd.sendCommand("CMD8")
     assert(v2card)
 
     status = "Busy"
     retries = 0
     while status == "Busy":
-        sendCommand(board, "CMD55")
-        status = sendCommand(board, "ACMD41 " + v2card)
+        sd.sendCommand("CMD55")
+        status = sd.sendCommand("ACMD41 " + v2card)
         retries += 1
         assert(retries < 10)
     assert(status == "Valid")
 
-    sdhc = sendCommand(board, "CMD58")
+    sdhc = sd.sendCommand("CMD58")
     assert(sdhc == "SDHC")
