d0192ee202f571bcf0267e1d9c0decae886edc6e
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2022-05-07 12:29:17 +0300

    Tests: store CMD respons (including time stats, logs, and arguments) in CmdResponse structure

--- Tests/protocol.py
+++ Tests/protocol.py
@@ -4,6 +4,16 @@ import re
 STATUS_LINE_RE = re.compile(r"(OK|ERROR) ?(.*)")
 TIMESTATS_LINE_RE = re.compile(r"TIMESTATS (\d+) (\d+) \((\d+)\)")
 
+class CmdResponse:
+    def __init__(self):
+        self.status = ""
+        self.value = ""
+        self.log = ""
+        self.tstart = 0
+        self.tend = 0
+        self.tdelta = 0
+
+
 class SdProtocol:
     def __init__(self):
         self.port = serial.Serial('COM6', baudrate=115200)
@@ -12,7 +22,7 @@ class SdProtocol:
     def sendCommand(self, cmd):
         self.port.write((cmd + "\n").encode())
 
-        response = ""
+        log = ""
         while True:
             line = self.port.readline().decode().rstrip()
             print("Received: " + line)
@@ -20,11 +30,14 @@ class SdProtocol:
             # Wait and parse status line
             m = STATUS_LINE_RE.match(line)
             if not m:
-                response += line + "\n"
+                log += line + "\n"
                 continue
 
-            status = m.group(1)
-            argument = m.group(2)
+            # Fill the response object
+            resp = CmdResponse()
+            resp.status = m.group(1)
+            resp.value = m.group(2)
+            resp.log = log
 
             # Wait for TIMESTATS
             line = self.port.readline().decode().rstrip()
@@ -33,15 +46,16 @@ class SdProtocol:
             if not m:
                 raise RuntimeError("Expecting TIMESTATS line")
 
-            tstart = m.group(1)
-            tend = m.group(2)
-            tdelta = m.group(3)
+            # Parse time stats data
+            resp.tstart = m.group(1)
+            resp.tend = m.group(2)
+            resp.tdelta = m.group(3)
 
-            if status == "OK":
-                return argument, response
+            if resp.status == "OK":
+                return resp
 
-            if status == "ERROR":
-                raise RuntimeError(argument)
+            if resp.status == "ERROR":
+                raise RuntimeError(resp.value)
 
 
 
@@ -55,33 +69,39 @@ class SdProtocol:
         self.sendCommand("CMD0")
     
     def cmd8(self):
-        v2card, response = self.sendCommand("CMD8")
-        assert("R7 = 000001aa" in response              # Normal R7 response
-               or "R1 = 05" in response                 # CMD8 will return illegal command in SPI mode for v1 cards
-               or "error code: 00000004" in response)   # CMD8 will timeout in SDIO mode for v1 cards
+        response = self.sendCommand("CMD8")
+        assert("R7 = 000001aa" in response.log              # Normal R7 response
+               or "R1 = 05" in response.log                 # CMD8 will return illegal command in SPI mode for v1 cards
+               or "error code: 00000004" in response.log)   # CMD8 will timeout in SDIO mode for v1 cards
+
+        # The value contains version of the card (1 or 2)
+        v2card = response.value 
         return int(v2card)
 
     def acmd41(self, hostSupportsSDHC):
         self.sendCommand("CMD55")
-        status, _ = self.sendCommand("ACMD41 "+ ("1" if hostSupportsSDHC else "0"))
-        return status
+        resp = self.sendCommand("ACMD41 "+ ("1" if hostSupportsSDHC else "0"))
+        return resp.value   # The value contains whether the card is Busy or Valid
 
     def cmd58(self):
-        sdhc, _ = self.sendCommand("CMD58")
-        return sdhc
+        resp = self.sendCommand("CMD58")
+        return resp.value   # The value contains whether the card is SDHC or SDSC
 
     def cmd2(self):
-        _, resp = self.sendCommand("CMD2")
+        resp = self.sendCommand("CMD2")
+        return resp.log     # The log contains a dump of CID register
 
     def cmd3(self):
-        status, resp = self.sendCommand("CMD3")
-        return status
+        resp = self.sendCommand("CMD3")
+        return resp.value   # Value contains RCA address
 
     def cmd7(self, rca):
-        status, resp = self.sendCommand("CMD7 " + str(rca))
+        self.sendCommand("CMD7 " + str(rca))
 
     def cmd9(self, rca):
-        status, resp = self.sendCommand("CMD9 " + str(rca))
+        resp = self.sendCommand("CMD9 " + str(rca))
+        return resp.log     # The log contains a dump of CSD register
 
     def cmd10(self, rca):
-        status, resp = self.sendCommand("CMD10 " + str(rca))
+        resp = self.sendCommand("CMD10 " + str(rca))
+        return resp.log     # The log contains a dump of CID register
